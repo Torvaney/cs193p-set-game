0d32bc2c7c089555b92e055f3564709f06fdbe2c
Ben Torvaney <torvaney@protonmail.com>
2022-03-05 21:00:27 +0000

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Ben Torvaney on 05/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Ben Torvaney on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
